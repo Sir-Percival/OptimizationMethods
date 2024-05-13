5f0e0a91b3a236d3b351ca5a177d8bd1eb2df5ec
Kacper Walichnowski <kacperw112@wp.pl>
2024-05-13 14:47:53 +0200

    Add printing step info to Nelder-Mead method

--- src/NelderMead.java
+++ src/NelderMead.java
@@ -9,7 +9,7 @@ public class NelderMead
     private static final double BETA = 0.5; // contraction coefficient
     private static final double GAMMA = 2.0; // expansion coefficient
     private static final double DELTA = 0.5; // reduction coefficient
-    private static final double EPSILON = 0.00000001; // tolerance
+    private static final double EPSILON = 1e-8; // tolerance
 
     private interface Function
     {
@@ -82,6 +82,8 @@ public class NelderMead
         double[][] simplex = buildSimplex(dimensions);
         double[] fValues = new double[dimensions + 1];
 
+        int iteration = 1;
+
         while (true)
         {
             // ========= 1 - Numbering of points =========
@@ -90,7 +92,16 @@ public class NelderMead
                 fValues[i] = function.calculate(simplex[i]);
             }
 
+            System.out.println("========== ITERATION #" + iteration + " ==========");
+            System.out.println("Simplex: " + Arrays.deepToString(simplex));
+            System.out.println("fValues: " + Arrays.toString(fValues));
+
             simplex = sortPoints(fValues, simplex);
+
+
+            System.out.println("Sorted simplex:");
+            System.out.println(Arrays.deepToString(simplex));
+            System.out.println("Sorted fValues: " + Arrays.toString(fValues));
 
             // ========= Calculate centroid =========
 
@@ -105,6 +116,9 @@ public class NelderMead
                 centroid[i] /= dimensions;
             }
 
+            System.out.println("===== CENTROID =====");
+            System.out.println("x0 = " + Arrays.toString(centroid));
+
             // ========= 2 - Find reflection point =========
 
             double[] reflected = new double[dimensions];
@@ -114,10 +128,15 @@ public class NelderMead
             }
             double fReflected = function.calculate(reflected);
 
+            System.out.println("===== REFLECTION =====");
+            System.out.println("Reflection point: " + Arrays.toString(reflected) + " = " + fReflected);
+
             // ========= 3 - If f(xr) < f(x1) find xe - expansion =========
 
             if(fReflected < fValues[0])
             {
+                System.out.println("===== (3) f(x0) < f(x1) -> EXPANSION =====");
+
                 double[] expanded = new double[dimensions];
                 for(int i=0; i<dimensions; i++)
                 {
@@ -125,16 +144,26 @@ public class NelderMead
                 }
                 double fExpanded = function.calculate(expanded);
 
+                System.out.println("Expansion point: " + Arrays.toString(expanded) + " = " + fExpanded);
+
                 // ========= 3A - If f(xe) < f(x1) - xn+1 = xe
                 if(fExpanded < fValues[0])
                 {
                     simplex[simplex.length - 1] = expanded;
                     fValues[fValues.length - 1] = fExpanded;
+                    System.out.println("===== (3A) f(xe) < f(x1) -> xn+1 = xe =====");
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
                 else    // ========= 3B - If f(xe) >= f(x1) - xn+1 = xr
                 {
                     simplex[simplex.length - 1] = reflected;
                     fValues[fValues.length - 1] = fReflected;
+                    System.out.println("===== (3B) f(xe) >= f(x1) -> xn+1 = xr =====");
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
             }
             // ========= 4 - If f(x1) <= f(xr) <= f(xn) - xn+1 = xr =========
@@ -142,6 +171,10 @@ public class NelderMead
             {
                 simplex[simplex.length - 1] = reflected;
                 fValues[fValues.length - 1] = fReflected;
+                System.out.println("===== (4) f(x1) <= f(xr) <= f(xn) -> xn+1 = xr =====");
+                System.out.println("New simplex:");
+                System.out.println(Arrays.deepToString(simplex));
+                System.out.println("New fValues: " + Arrays.toString(fValues));
             }
             // ========= 5 - If f(xn) < f(xr) < f(xn+1) - xn+1 = xr + Contraction =========
             else if(fValues[fValues.length-2] < fReflected && fReflected < fValues[fValues.length-1])
@@ -149,6 +182,11 @@ public class NelderMead
                 simplex[simplex.length - 1] = reflected;
                 fValues[fValues.length - 1] = fReflected;
 
+                System.out.println("===== (5) f(xn) < f(xr) < f(xn+1) -> xn+1 = xr =====");
+                System.out.println("New simplex:");
+                System.out.println(Arrays.deepToString(simplex));
+                System.out.println("New fValues: " + Arrays.toString(fValues));
+
                 double[] contraction = new double[dimensions];
                 for(int i=0; i<dimensions; i++)
                 {
@@ -156,15 +194,23 @@ public class NelderMead
                 }
                 double fContraction = function.calculate(contraction);
 
+                System.out.println("===== CONTRACTION =====");
+                System.out.println("Contraction point: " + Arrays.toString(contraction) + " = " + fContraction);
+
                 // ========== 5A - If f(xc) < f(xn+1) - xn+1 = xc ==========
                 if(fContraction < fValues[fValues.length-1])
                 {
+                    System.out.println("===== (5A) f(xc) < f(xn+1) -> xn+1 = xc =====");
                     simplex[simplex.length - 1] = contraction;
                     fValues[fValues.length - 1] = fContraction;
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
                 // ========== 5B - Else - Reduction ==========
                 else
                 {
+                    System.out.println("===== (5B) REDUCTION =====");
                     for(int i=0; i <= dimensions; i++)
                     {
                         for(int j=0; j < dimensions; j++)
@@ -173,11 +219,16 @@ public class NelderMead
                         }
                         fValues[i] = function.calculate(simplex[i]);
                     }
+
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
             }
             // ========== 6 - If f(xr) >= f(xn+1) - Contraction ==========
             else
             {
+                System.out.println("===== (6) f(xr) >= f(xn+1) -> CONTRACTION =====");
                 double[] contraction = new double[dimensions];
                 for(int i=0; i<dimensions; i++)
                 {
@@ -185,15 +236,22 @@ public class NelderMead
                 }
                 double fContraction = function.calculate(contraction);
 
+                System.out.println("Contraction point: " + Arrays.toString(contraction) + " = " + fContraction);
+
                 // ========== 6A - If f(xc) < f(xn+1) - xn+1 = xc ==========
+                System.out.println("===== (6A) f(xc) < f(xn+1) -> xn+1 = xc =====");
                 if(fContraction < fValues[fValues.length-1])
                 {
                     simplex[simplex.length - 1] = contraction;
                     fValues[fValues.length - 1] = fContraction;
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
                 // ========== 6B - Else - Reduction ==========
                 else
                 {
+                    System.out.println("===== (6B) REDUCTION =====");
                     for(int i=0; i <= dimensions; i++)
                     {
                         for(int j=0; j < dimensions; j++)
@@ -202,6 +260,10 @@ public class NelderMead
                         }
                         fValues[i] = function.calculate(simplex[i]);
                     }
+
+                    System.out.println("New simplex:");
+                    System.out.println(Arrays.deepToString(simplex));
+                    System.out.println("New fValues: " + Arrays.toString(fValues));
                 }
             }
 
@@ -214,7 +276,14 @@ public class NelderMead
                 value += Math.pow(fValues[i] - fCentroid, 2);
             }
 
+            System.out.println("===== STOP CRITERION =====");
+
             value = Math.sqrt(value / (dimensions+1));
+
+            System.out.println("Best point: " + Arrays.toString(simplex[0]) + " = " + fValues[0]);
+            System.out.println("Value = " + value + " (< " + EPSILON + ")");
+            System.out.println("========== END OF ITERATION ==========\n");
+            iteration++;
 
             if(value <= EPSILON)
             {
@@ -223,7 +292,7 @@ public class NelderMead
         }
 
         double[] minimum = simplex[0];
-        System.out.println("Minimum point: " + Arrays.toString(minimum));
+        System.out.println("Final simplex: " + Arrays.deepToString(simplex));
         System.out.printf("Minimum value: %.15f%n", fValues[0]);
     }
 
